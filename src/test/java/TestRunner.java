7cc08dc5ebbce762382e042c37bab8c9310cc37a
FarzanaRitu <farzanaritusha@gmail.com>
2024-10-01 18:04:54 +0600

    first commit

---
+++ src/test/java/TestRunner.java
@@ -0,0 +1,11 @@
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "src/test/resources/features",
+    glue = "stepDefinitions" // Ensure this is the correct package name
+)
+public class TestRunner {
+}
---
+++ target/test-classes/features/example.feature
@@ -0,0 +1,4 @@
+Feature: Example feature
+  Scenario: Print a hello message
+    Given I want to print hello
+    Then I print "Hello, Cucumber!"
